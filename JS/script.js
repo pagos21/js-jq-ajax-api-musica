91ddbc5a1fc23f8d67af8057f7bbab5328cc0329
pagos21 <paga@localhost.yeah>
2020-07-22 16:53:12 +0200

    v 1.0

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ function appd(){
   var compiled = Handlebars.compile(template);
 
   var context = {"poster":"posters2", "title":"titles2", "author":"authors2", "year":"years2"}
-  var target = $("#cd");
+  var target = $("#cds-container");
   var html = compiled(context);
   target.append(html);
 
@@ -15,19 +15,30 @@ function getApi(){
   $.ajax({
     url : "https://flynn.boolean.careers/exercises/api/array/music",
     method: 'GET',
-    success: function(data, state) {
+    success: function(data, state) { //Al termine della connessione al server (success)
       var success = data['success'];
       var value = data['response'];
-      var titles = value.map(anon => anon.title);
-      var posters = value.map(anon => anon.poster);
-      var authors = value.map(anon => anon.author);
-      var genre = value.map(anon => anon.genre);
-      var year = value.map(anon => anon.year);
+
+      // Handlebars
+      var template = $("#template").html();
+      var compiled = Handlebars.compile(template);
+      var target = $(".cds-container");
+
+      for (var i = 0; i < value.length; i++) {
+        console.log(value[i]);
+        var context = compiled(value[i]);
+        target.append(context);
+      }
+
+      // var titles = value.map(anon => anon.title);
+      // var posters = value.map(anon => anon.poster);
+      // var authors = value.map(anon => anon.author);
+      // var genre = value.map(anon => anon.genre);
+      // var year = value.map(anon => anon.year);
 
 
 
       if (success) {
-        console.log(titles, authors);
 
       } else {
         console.log("Error");
@@ -44,7 +55,6 @@ function getApi(){
 
 function init (){
 getApi();
-appd();
 }
 
 
